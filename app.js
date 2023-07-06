456ef850f64f994bd59fe243d58c5f17b9e2a032
miltonjrr <miltonnasarojr@gmail.com>
2023-07-06 19:01:05 -0300

    personal changes

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ function toggleMode() {
   //switch image
   if (html.classList.contains("light")) {
     //image with lightmode on
-    img.setAttribute("src", "./assets/avatar-light.png") //with setAttibute we can change the attribute we set o the html file
+    img.setAttribute("src", "./assets/personal-avatar-light.png") //with setAttibute we can change the attribute we set o the html file
     img.setAttribute("alt", "Imagem so maiky de oculos escuros.")
   } else {
     //image with lightmode off
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/personal-avatar.png")
     img.setAttribute("alt", "Imagem do maiky sorrindo e com fundo amarelo.")
   }
 }
